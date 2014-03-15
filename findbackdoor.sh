c94d524a1e54e7e283f3ec881ce4fcc4b71c3095
Roberto Soares Espreto <robertoespreto@gmail.com>
2014-03-15 17:19:52 -0300

    Modified findbackdoor.sh

--- findbackdoor.sh
+++ findbackdoor.sh
@@ -8,18 +8,18 @@ LOGWEB="logs/find-webshells.$DATA.log"
 LOGFUNC="logs/find-functions.$DATA.log"
 
 if [ "$OPTION" = 1 ]; then
-    echo "[+] Possible webshell found! ""\n"
+    echo "[+] Possible webshell found! "
     find "$DIR" -iname "*.php" -type f | grep -if $(pwd)/assinaturas_webshells.txt 2>/dev/null | tee "$LOGWEB"
     echo
 
 elif [ "$OPTION" = 2 ]; then
-    echo "[+] Possible danger function!""\n"
+    echo "[+] Possible dangerous function!"
     find "$DIR" -iname "*.php" -type f -exec sh -c 'fgrep -q -C5 -n -i -f assinaturas_funcoes.txt {} && echo "{}\n"; fgrep -C5 -n -i -f assinaturas_funcoes.txt {}' \; | tee "$LOGFUNC"
 
 else
-    echo " Opção Inválida!"
+    echo " Invalid option!"
     echo
-    echo " Use: ./findbackdoor.sh <diretorio para o scan> <1 ou 2>"
+    echo " Use: ./findbackdoor.sh <directory scan> <1 or 2>"
     echo "  1 = WebShells"
     echo "  2 = Functions"
     echo
