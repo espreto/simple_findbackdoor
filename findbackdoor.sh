b84f571ee919cd015ada433e0fbeac3f32a14a75
Roberto Soares Espreto <robertoespreto@gmail.com>
2014-10-15 11:49:29 -0300

    Indentation modified.

--- findbackdoor.sh
+++ findbackdoor.sh
@@ -8,21 +8,21 @@ LOGWEB="logs/find-webshells.$DATA.log"
 LOGFUNC="logs/find-functions.$DATA.log"
 
 if [ "$OPTION" = 1 ]; then
-    echo "[+] Possible webshell found! "
-    find "$DIR" -iname "*.php" -type f | grep -if $(pwd)/assinaturas_webshells.txt 2>/dev/null | tee "$LOGWEB"
-    echo
+  echo "[+] Possible webshell found! "
+  find "$DIR" -iname "*.php" -type f | grep -if $(pwd)/assinaturas_webshells.txt 2>/dev/null | tee "$LOGWEB"
+  echo
 
 elif [ "$OPTION" = 2 ]; then
-    echo "[+] Possible dangerous function!"
-    find "$DIR" -iname "*.php" -type f -exec sh -c 'fgrep -q -C5 -n -i -f assinaturas_funcoes.txt {} && echo "{}\n"; fgrep -C5 -n -i -f assinaturas_funcoes.txt {}' \; | tee "$LOGFUNC"
+  echo "[+] Possible dangerous function!"
+  find "$DIR" -iname "*.php" -type f -exec sh -c 'fgrep -q -C5 -n -i -f assinaturas_funcoes.txt {} && echo "{}\n"; fgrep -C5 -n -i -f assinaturas_funcoes.txt {}' \; | tee "$LOGFUNC"
 
 else
-    echo " Invalid option!"
-    echo
-    echo " Use: ./findbackdoor.sh <directory scan> <1 or 2>"
-    echo "  1 = WebShells"
-    echo "  2 = Functions"
-    echo
+  echo " Invalid option!"
+  echo
+  echo " Use: ./findbackdoor.sh <directory scan> <1 or 2>"
+  echo "  1 = WebShells"
+  echo "  2 = Functions"
+  echo
 fi
 
 exit 0
